c9294e6481a8dd1a2a11ab717dc9872e171b3ec2
weiensong <61647484+weiensong@users.noreply.github.com>
2023-08-15 17:45:22 +0800

    refactor: login_guat.bash

--- login_guat.bash
+++ login_guat.bash
@@ -1,3 +1,8 @@
- curl -X POST -d "DDDDD=你的账号&upass=你的密码&R1=0&R3=1&R6=0&para=00&v6ip=_:1574659637705" http://10.1.2.3
+username=''
 
- 
+password=''
+
+# 0: 校园网,1: 电信,2: 联通,3: 移动
+serviceProvider='1'
+
+curl -X POST -d "DDDDD=$username&upass=$password&R1=0&R3=$serviceProvider&R6=0&para=00&v6ip=_:1574659637705" http://10.1.2.3
